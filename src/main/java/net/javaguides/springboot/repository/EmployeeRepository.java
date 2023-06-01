108b701b26b9d1c4b6790c7211d126a606fea0d1
Dan-Sanch <59940500+Dan-Sanch@users.noreply.github.com>
2023-06-01 15:55:59 -0600

    Implemented JPQL query with named parameters

--- src/main/java/net/javaguides/springboot/repository/EmployeeRepository.java
+++ src/main/java/net/javaguides/springboot/repository/EmployeeRepository.java
@@ -3,12 +3,16 @@ package net.javaguides.springboot.repository;
 import net.javaguides.springboot.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
     Optional<Employee> findByEmail(String email);
 
-    @Query("select e from Employee e where e.firstName=?1 and e.lastName=?2")
+    @Query("select e from Employee e where e.firstName = ?1 and e.lastName = ?2")
     Employee findByJpql(String firstName, String lastName);
+
+    @Query("select e from Employee e where e.firstName = :firstNameParam and e.lastName = :lastNameParam")
+    Employee findByJpqlNamedParams(@Param("firstNameParam") String firstName, @Param("lastNameParam") String lastName);
 }
