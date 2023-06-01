9815937a3c27b517dad25b43770ddee955a6e424
Dan-Sanch <59940500+Dan-Sanch@users.noreply.github.com>
2023-06-01 15:40:52 -0600

    Implemented JPQL query in EmployeeRepository

--- src/main/java/net/javaguides/springboot/repository/EmployeeRepository.java
+++ src/main/java/net/javaguides/springboot/repository/EmployeeRepository.java
@@ -2,9 +2,13 @@ package net.javaguides.springboot.repository;
 
 import net.javaguides.springboot.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
     Optional<Employee> findByEmail(String email);
+
+    @Query("select e from Employee e where e.firstName=?1 and e.lastName=?2")
+    Employee findByJpql(String firstName, String lastName);
 }
