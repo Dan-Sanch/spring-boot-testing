a7a5c6495943026805b7cc8f856027468b7bd3d8
Dan-Sanch <59940500+Dan-Sanch@users.noreply.github.com>
2023-06-01 14:00:23 -0600

    Created test for findAll() Employees

--- src/test/java/net/javaguides/springboot/repository/EmployeeRepositoryTest.java
+++ src/test/java/net/javaguides/springboot/repository/EmployeeRepositoryTest.java
@@ -7,6 +7,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import java.util.List;
+
 @DataJpaTest
 public class EmployeeRepositoryTest {
     @Autowired
@@ -29,5 +31,31 @@ public class EmployeeRepositoryTest {
         // then - verify the output
         assertThat(savedEmployee).isNotNull();
         assertThat(savedEmployee.getId()).isGreaterThan(0);
+    }
+
+    @DisplayName("Get all employees test")
+    @Test
+    public void givenEmployeesList_whenFindAll_thenReturnAllEmployees() {
+        // given - precondition or setup
+        Employee employee1 = Employee.builder()
+                .firstName("Dan")
+                .lastName("Sanchez")
+                .email("dan@domain.com")
+                .build();
+        Employee employee2 = Employee.builder()
+                .firstName("John")
+                .lastName("Cena")
+                .email("john@domain.com")
+                .build();
+        employeeRepository.save(employee1);
+        employeeRepository.save(employee2);
+
+        // when - action or the behavior that we are going to test
+        List<Employee> employees = employeeRepository.findAll();
+
+        // then - verify the output
+        assertThat(employees).isNotNull();
+        assertThat(employees.size()).isEqualTo(2);
+        assertThat(employees).contains(employee1, employee2);
     }
 }
