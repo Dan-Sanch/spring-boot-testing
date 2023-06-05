fdf1fc9e0476cdce0cac277f03106115ed7bfc41
Dan-Sanch <59940500+Dan-Sanch@users.noreply.github.com>
2023-06-05 12:58:45 -0600

    Implemented test that verifies an exception is thrown, and save() is never called

--- src/test/java/net/javaguides/springboot/service/EmployeeServiceTests.java
+++ src/test/java/net/javaguides/springboot/service/EmployeeServiceTests.java
@@ -1,14 +1,22 @@
 package net.javaguides.springboot.service;
 
+import net.javaguides.springboot.exception.ResourceNotFoundException;
 import net.javaguides.springboot.model.Employee;
 import net.javaguides.springboot.repository.EmployeeRepository;
 import net.javaguides.springboot.service.impl.EmployeeServiceImpl;
 import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -56,5 +64,23 @@ public class EmployeeServiceTests {
 
         // Then
         assertThat(savedEmployee).isNotNull();
+    }
+
+    @Test
+    @DisplayName("saveEmployee Exception test")
+    public void givenExistingEmail_whenSaveEmployee_thenThrowsException() {
+        // Given
+        // Method stubbing:
+        given(employeeRepository.findByEmail(employee.getEmail()))
+                .willReturn(Optional.of(employee))
+        ;
+
+        // When
+        Assertions.assertThrows(ResourceNotFoundException.class, () -> {
+            employeeService.saveEmployee(employee);
+        });
+
+        // Then
+        verify(employeeRepository, never()).save(any(Employee.class));
     }
 }
